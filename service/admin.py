51cab1c20d013fb1c3b20909786d2019c562c496
Denys Halytskyi <denys.halytskyi17@gmail.com>
2026-08-18 21:22:42 +0300

    add admin panel

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer, Vehicle, Part, Service, ServiceOrder, UsedPart
 
-# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Vehicle)
+admin.site.register(Part)
+admin.site.register(Service)
+admin.site.register(ServiceOrder)
+admin.site.register(UsedPart)
