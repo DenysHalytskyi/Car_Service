b8b597d868d1f9aa30c2ee3abe3d0b99b69612d5
Denys Halytskyi <denys.halytskyi17@gmail.com>
2026-08-19 21:00:17 +0300

    Add CORS support and custom order endpoints for total cost and status

--- Car_Service/settings.py
+++ Car_Service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
     'service',
 ]
 
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -124,6 +126,8 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Static files (CSS, JavaScript, Images)
--- service/views.py
+++ service/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum, F
 from .models import Customer, Vehicle, Part, Service, ServiceOrder, UsedPart
 from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import (
@@ -32,6 +35,38 @@ class ServiceViewSet(viewsets.ModelViewSet):
 class ServiceOrderViewSet(viewsets.ModelViewSet):
     queryset = ServiceOrder.objects.all()
     serializer_class = ServiceOrderSerializer
+
+    @action(detail=True, methods=['get'], url_path='total-cost')
+    def calculate_total_cost(self, request, pk=None):
+        #Total
+        order = self.get_object()
+        #All services
+        services_cost = order.services.aggregate(total=Sum('price'))['total'] or 0
+        #Spare parts used
+        parts_cost = order.used_parts.aggregate(total=Sum(F('quantity') * F('part__price')))['total'] or 0
+
+        total_cost = services_cost + parts_cost
+
+        return Response({
+            'order_id': order.id,
+            'services_cost': services_cost,
+            'parts_cost': parts_cost,
+            'total_cost': total_cost
+        }, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['patch'], url_path='change-status')
+    def change_status(self, request, pk=None):
+        order = self.get_object()
+        new_status = request.data.get('status')
+
+        if new_status not in ServiceOrder.OrderStatus.values:
+            return Response({'error': 'Invalid status choice.'},
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
+
+        order.status = new_status
+        order.save()
+        return Response({'status': f'Order status updated to {order.status}'})
 
 
 class UsedPartViewSet(viewsets.ModelViewSet):
